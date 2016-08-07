ea840e4b9466ac15ed44dde82533afa4f1aec559
janak panchal <janakpanchal13@gmail.com>
2016-08-07 10:14:49 +0530

    Add files via upload

---
+++ cont.php
@@ -0,0 +1,33 @@
+<?php
+   
+
+   <center> <h4 class="glyphicon glyphicon-envelope"> Contact us </h4> </center>
+	      <form role="form">
+		   <div class="form-group">
+      <label for="email">Name :</label>
+      <input type="email" class="form-control" id="name" placeholder="Name ">
+    </div>
+	
+	
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+	 
+	  <div class="form-group">
+      <label for="tel">Phone No:</label>
+      <input type="tel" class="form-control" id="phone_no" placeholder="Phone No">
+    </div>
+	
+		  <div class="form-group">
+      <label for="message-box">Message</label>
+      <input type="textarea" class="form-control" id="message-box" placeholder="Message ">
+    </div>
+   
+   
+	<br>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+	  
+
+	  ?>
